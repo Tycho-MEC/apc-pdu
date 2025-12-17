e4f6b000d8665b547df0dc3e64b93b8df354b509
Tycho MEC <davidxstott@protonmail.com>
2025-12-17 17:43:06 +0000

    Added config_schema for validation

--- custom_components/apc_pdu/__init__.py
+++ custom_components/apc_pdu/__init__.py
@@ -4,7 +4,10 @@ import logging
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.typing import ConfigType
+from homeassistant.helpers import config_validation as cv
 from .const import DOMAIN
+
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
 _LOGGER = logging.getLogger(__name__)
 
